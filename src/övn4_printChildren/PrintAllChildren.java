05adb2956db8faca7e2067f57795d3ecba603d20
sigrunolafsdottir <solafsdottir@gmail.com>
2023-01-11 17:32:31 +0100

    Added alternate solution to övn 4

--- src/övn4_printChildren/PrintAllChildren.java
+++ src/övn4_printChildren/PrintAllChildren.java
@@ -1,18 +1,25 @@
 package övn4_printChildren;
 
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 import övn4_printChildren.Models.Child;
+import övn4_printChildren.Models.Present;
 
 
 public class PrintAllChildren {
     
     public static void main (String args[]){
         Repository r = new Repository();
-        r.getAllChildren().forEach(c -> c.print());
+        List<Child> l = r.getAllChildren();
+        l.forEach(c -> c.print());
 
         System.out.println(" ");
         AlternativeSolutionRepo ar = new AlternativeSolutionRepo();
-        ar.getAllChildren().forEach((k,v) -> v.print());
+        Map<Integer, Child> cMap = ar.getAllChildren();
+        cMap.forEach((k,v) -> v.print());
+
     }
 
 }
